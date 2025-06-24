e982e24c1096d3d88ae265fbd357cdcb60614a0b
Flimbo _ <supergameboy87@gmail.com>
2025-06-23 20:37:47 -0600

    VIRTUAL INSADNITY :DDDDDDDDD

--- other/materialsrc/vtex_compile_Jazz.bat
+++
@@ -1,3 +0,0 @@
-for /d %%G in ("..\materialsrc\models\andy\*") do (
-	call "..\..\bin\vtex" -mkdir -nopause -outdir "..\materials\models\andy\%%~nxG" "..\materialsrc\models\andy\%%~nxG\*.tga" )
-pause
